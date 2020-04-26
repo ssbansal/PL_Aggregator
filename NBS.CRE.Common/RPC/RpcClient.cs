faaffcf48defb640ba57839b4da963025b33aef6
ssbansal <bansal.sarbjeet@gmail.com>
2020-04-26 20:29:26 +0530

    Changed message durability to non-persistent

--- NBS.CRE.Common/RPC/RpcClient.cs
+++ NBS.CRE.Common/RPC/RpcClient.cs
@@ -80,6 +80,7 @@ namespace NBS.CRE.Common.RPC
 
             var bytes = SerializationHelper.ToBytes(request);
             var props = channel.CreateBasicProperties();
+            props.DeliveryMode = 1;
             props.CorrelationId = correlationId;
             props.ReplyTo = ReplyQueueName;
 
